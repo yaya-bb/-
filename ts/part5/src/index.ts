2edd4e3c464d98080711b0a638f7c1b675ccb0f9
yaya-bb <2286834433@qq.com>
2022-09-30 22:41:49 +0800

    修改

--- ts/part5/src/index.ts
+++ ts/part5/src/index.ts
@@ -1,3 +1,11 @@
-// 引入样式
-import './style/index.less';
-console.log(123);
+import './style/index.less'
+// import Food from './modules/Food'
+// import ScorePanel from './modules/ScorePanel'
+ 
+// const food=new Food;
+// console.log(food.X,food.Y);
+// food.change();
+// console.log(food.X,food.Y);
+
+// const scorePanel=new ScorePanel()
+// scorePanel.addScore()
