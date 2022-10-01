77449ba494b0c7ca881bc480734073ddbf10d165
yaya-bb <2286834433@qq.com>
2022-10-02 06:21:33 +0800

    项目界面

--- ts/part5/src/index.js
+++ ts/part5/src/index.js
@@ -1,3 +1,1 @@
-// 引入样式
 import './style/index.less';
-console.log(123);
--- ts/part5/src/index.ts
+++ ts/part5/src/index.ts
@@ -1,11 +1,1 @@
 import './style/index.less'
-// import Food from './modules/Food'
-// import ScorePanel from './modules/ScorePanel'
- 
-// const food=new Food;
-// console.log(food.X,food.Y);
-// food.change();
-// console.log(food.X,food.Y);
-
-// const scorePanel=new ScorePanel()
-// scorePanel.addScore()
