3450d7f3ca42c6b5937d7352d6301c6944ff5cf3
yaya-bb <2286834433@qq.com>
2022-10-02 07:43:13 +0800

    完成ScorePanel类

---
+++ ts/part5/modules/ScorePanel.ts
@@ -0,0 +1,43 @@
+class ScorePanel {
+  // score和level用来记录分数和等级
+  score = 0;
+  level = 1;
+  // 分数和等级所在的元素，在构造函数中进行初始化
+  scoreEle: HTMLElement;
+  levelEle: HTMLElement;
+  // 设置一个变量限制等级
+  maxLevel: number;
+  // 设置一个变量表示多少分时进行升级
+  upScore: number;
+  // 构造函数
+  // 参数带值则表示默认值，如果没有传参则使用默认值
+  constructor(maxLevel: number = 10, upScore: number = 10) {
+    this.scoreEle = document.getElementById("score")!;
+    this.levelEle = document.getElementById("level")!;
+    this.maxLevel = maxLevel;
+    this.upScore = upScore;
+  }
+  // 设置一个加分的方法
+  addScore() {
+    // 使分数自增
+    this.scoreEle.innerHTML = ++this.score + "";
+    // 判断分数是多少
+    if(this.score % this.upScore === 0) {
+      this.levelUp();
+    }
+  }
+  // 提升等级的方法
+  levelUp() {
+    if (this.level < this.maxLevel) {
+      this.levelEle.innerHTML = ++this.level + "";
+
+    }
+  }
+}
+const scorePanel = new ScorePanel(15, 12);
+scorePanel.addScore();
+for(let i = 0; i < 20; i++) {
+  scorePanel.addScore();
+}
+
+export default ScorePanel;
