a5b95a98cf70596f32d0e2e843bd065aa544dacc
yaya-bb <2286834433@qq.com>
2022-10-12 16:12:51 +0800

    feat: 手写实现Scannner类

--- mustache-study/templateengine/src/index.js
+++ mustache-study/templateengine/src/index.js
@@ -1,1 +1,22 @@
-console.log(1111)
+// 调包
+import Scanner from './Scanner.js';
+// 全局提供templateEngine对象
+window.templateEngine = {
+  // 渲染方法
+  render(templateStr, data) {
+    // 引入
+    // 实例化一个扫描器，构造时候提供一个参数，这个参数就是模版字符串
+    // 也就是说这个扫描器就是针对这个模版字符串工作的
+    var scanner = new Scanner(templateStr);
+    // 当不等于模版字符串最后的时候一直进行循环，直到到达则结束
+    var word;
+    while(!scanner.eos()) {
+      word = scanner.scanUtil('{{');
+      console.log(word);
+      scanner.scan('{{');
+      word = scanner.scanUtil('}}');
+      console.log(word);
+      scanner.scan('}}');
+    }
+  }
+}
