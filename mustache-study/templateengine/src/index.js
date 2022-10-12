9ccc0783faea7d6df8791270eebd3670880dd2bd
yaya-bb <2286834433@qq.com>
2022-10-12 17:07:32 +0800

    feat:手写将HTML变为tokens

--- mustache-study/templateengine/src/index.js
+++ mustache-study/templateengine/src/index.js
@@ -1,22 +1,26 @@
 // 调包
-import Scanner from './Scanner.js';
+import parseTemplateToTokens from './parseTemplateToTokens.js'; 
 // 全局提供templateEngine对象
 window.templateEngine = {
-  // 渲染方法
+  // // 渲染方法
+  // render(templateStr, data) {
+  //   // 引入
+  //   // 实例化一个扫描器，构造时候提供一个参数，这个参数就是模版字符串
+  //   // 也就是说这个扫描器就是针对这个模版字符串工作的
+  //   var scanner = new Scanner(templateStr);
+  //   // 当不等于模版字符串最后的时候一直进行循环，直到到达则结束
+  //   var word;
+  //   while(!scanner.eos()) {
+  //     word = scanner.scanUtil('{{');
+  //     console.log(word);
+  //     scanner.scan('{{');
+  //     word = scanner.scanUtil('}}');
+  //     console.log(word);
+  //     scanner.scan('}}');
+  //   }
   render(templateStr, data) {
-    // 引入
-    // 实例化一个扫描器，构造时候提供一个参数，这个参数就是模版字符串
-    // 也就是说这个扫描器就是针对这个模版字符串工作的
-    var scanner = new Scanner(templateStr);
-    // 当不等于模版字符串最后的时候一直进行循环，直到到达则结束
-    var word;
-    while(!scanner.eos()) {
-      word = scanner.scanUtil('{{');
-      console.log(word);
-      scanner.scan('{{');
-      word = scanner.scanUtil('}}');
-      console.log(word);
-      scanner.scan('}}');
-    }
+    // 调用parseTemplateToTokens函数，让模版字符串能够变为tokens数组
+    var tokens = parseTemplateToTokens(templateStr);
+    console.log(tokens);
   }
 }
---
+++ mustache-study/templateengine/src/parseTemplateToTokens.js
@@ -0,0 +1,35 @@
+import Scanner from './Scanner.js';
+// 向外默认暴露这个函数
+// 将模板字符串变为tokens数组
+export default function parseTemplateToTokens(templateStr) {
+  var tokens = [];
+  // 创建扫描器
+  var scanner = new Scanner(templateStr);
+  var words;
+  // 让扫描器工作
+  while (!scanner.eos()) {
+    // 收集开始标记出现之前的文字
+    words = scanner.scanUtil('{{');
+    if(words != '') {
+      // 存起来
+      tokens.push(['text', words]);
+    }
+    // 过双大括号
+    scanner.scan('{{');
+    // 收集开始标记出现之前的文字
+    words = scanner.scanUtil('}}');
+    if(words != '') {
+      // 这个words就是{{}}中间的东西，判断一下首字符
+      if(words[0] == "#") {
+        // 存起来，从下标为1的项开始存,因为下标为0的项是#
+        tokens.push(['#', words.substring(1)]);
+      }else if(words[0] == "/") {
+        tokens.push(['/', words.substring(1)]);
+      } else {
+        tokens.push(['text', words]);
+      }
+    }
+    scanner.scan('}}');
+  }
+  return tokens;
+}
