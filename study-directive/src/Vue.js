7f685777b5568c3add4fd50db5031e88aad56212
yaya-bb <2286834433@qq.com>
2022-10-31 12:50:32 +0800

    feat:Vue类的创建

---
+++ study-directive/src/Vue.js
@@ -0,0 +1,13 @@
+import Compile from "./Compile";
+
+export default class Vue {
+  constructor(options) {
+    // 把参数options对象存为$options
+    this.$options = options || {};
+    // 数据
+    this._data = options.data || undefined;
+    // 数据变为响应式
+    // 模板编译
+    new Compile(options.el, this);
+  }
+}
---
+++ study-directive/src/index.js
@@ -0,0 +1,3 @@
+import Vue from './Vue.js';
+// 直接从全局要Vue的作用域， 所以使用window用于暴露
+window.Vue = Vue;
