804c9e0237b904713eb2d3100f26b9a04b86a01f
yaya-bb <2286834433@qq.com>
2022-11-07 21:58:27 +0800

    手写新旧节点的text不同

--- study-snabbdom/src/index.js
+++ study-snabbdom/src/index.js
@@ -1,7 +1,7 @@
 import h from './mysnabbdom/h.js';
 import patch from './mysnabbdom/patch.js'
 
-var myVnode = h('ul', {}, [
+var myVnode = h('selection', {}, [
   h('li', {}, '哈哈'),
   h('li', {}, '嘻嘻'),
 ]);
@@ -9,10 +9,7 @@ const container = document.getElementById('container');
 const btn = document.getElementById('btn');
 patch(container, myVnode);
 
-var myVnode1 = h('section', {}, [
-  h('h1', {}, '你好'),
-  h('h2', {}, '学校'),
-]);
+var myVnode1 = h('section', {}, '你好');
 btn.onclick = function() {
   patch(myVnode, myVnode1);
 }
--- study-snabbdom/src/mysnabbdom/patch.js
+++ study-snabbdom/src/mysnabbdom/patch.js
@@ -10,6 +10,17 @@ export default function(oldVnode, newVnode) {
   // 判断oldVnode和newVnode是不是同一个节点
   if(oldVnode.key == newVnode.key && oldVnode.sel == newVnode.sel) {
     // 精细比较
+    // 判断新旧vnode是否是同一个对象
+    if(oldVnode == newVnode) return;
+    // 判断新vnode有没有text属性
+    if(newVnode.text != undefined && (newVnode.children == undefined || newVnode.children.length == 0)) {
+      if(newVnode.text != oldVnode.text) {
+        oldVnode.elm.innerText = newVnode.text;
+      } 
+    } else {
+      // 新vnode没有text属性
+      // console.log('新vnode没有text属性');
+    }
   } else {
     console.log('暴力插入新的，删除旧的');
     // 需要.elm得到  
