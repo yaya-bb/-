850613d837f48c5c611aff4f9445035986fdc309
yaya-bb <2286834433@qq.com>
2022-11-08 08:40:45 +0800

    手写新旧节点text不同的情况

--- study-snabbdom/src/index.js
+++ study-snabbdom/src/index.js
@@ -1,15 +1,15 @@
 import h from './mysnabbdom/h.js';
 import patch from './mysnabbdom/patch.js'
 
-var myVnode = h('selection', {}, [
-  h('li', {}, '哈哈'),
-  h('li', {}, '嘻嘻'),
-]);
+var myVnode = h('selection', {}, '你好');
 const container = document.getElementById('container');
 const btn = document.getElementById('btn');
 patch(container, myVnode);
 
-var myVnode1 = h('section', {}, '你好');
+var myVnode1 = h('section', {}, [
+  h('li', {}, '哈哈'),
+  h('li', {}, '嘻嘻'),
+]);
 btn.onclick = function() {
   patch(myVnode, myVnode1);
 }
